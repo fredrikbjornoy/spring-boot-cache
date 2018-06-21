c48244c3613bbcb830fd29d8fd9b6a86f32c11b8
Fredrik Bjornoy <fredrik.bjornoy@gmail.com>
2018-06-21 20:04:21 +0200

    Created custom CacheManager for Caffeine

--- src/main/java/net/bjornoy/cache/config/CacheConfig.java
+++ src/main/java/net/bjornoy/cache/config/CacheConfig.java
@@ -1,10 +1,37 @@
 package net.bjornoy.cache.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    private final String name;
+    private final Long ttl;
+
+    public CacheConfig(@Value("${cache.name}") String name,
+                       @Value("${cache.ttl}") Long ttl) {
+        this.name = name;
+        this.ttl = ttl;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
+                .expireAfterWrite(ttl, TimeUnit.SECONDS)
+                .maximumSize(100);
+
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager(name);
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
 
 }
